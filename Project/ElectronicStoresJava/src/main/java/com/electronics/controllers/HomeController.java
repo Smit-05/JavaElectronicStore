c98b2f6a508f6fa0ed739d065b2bbb226ecc1082
Smit <smitpadaliya5@gmail.com>
2022-03-08 19:21:55 +0530

    first commit

---
+++ Project/ElectronicStoresJava/src/main/java/com/electronics/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.electronics.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String main() {
+		return "main-menu";
+	}
+}
